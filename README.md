584eded00a05a0bd43603736cd9a602596c49e8a
Alexey Gogolev <31016935+alexeygogolev@users.noreply.github.com>
2026-04-30 10:40:31 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Проверка значения из БД Битрикса (конфиг .settings.php)
+
+Скрипт читает конфиг сайта Битрикса .settings.php ( <path_to_bxdb_config> ), делает запрос ( <single_num_value_query> ) к его БД, сравнивает результат возвращенный запросом с заданной величиной ( <max_num_value> ).
+Если результат больше заданной величины, выполняется выход с кодом ошибки (иначе нормальное завершение).
+
+Может быть полезен для проверки неотправленных сообщений в БД Битрикс, особено с утилитой [monit](https://mmonit.com/monit/).
+
+### Использование:
+```console
+$ check_bx_db_value-v2.sh <path_to_bxdb_config> <single_num_value_query> <max_num_value>
+```
+### Пример вызова (содержимое скрипта ab_cd_unsent_check.sh):
+```bash
+check_bx_db_value-v2.sh "/www/ab.cd/bitrix/.settings.php" "select count(id) from b_event where SUCCESS_EXEC<>'Y'" 2
+```
+### Пример конфигурации для Monit:
+
+```bash
+check program ab_cd_unsent_check with path /home/bitrix/scripts/ab_cd_unsent_check.sh
+every 2 cycles
+    group mail
+if status != 0 then alert
+```
+### Пример вывода Monit:
+![Monit output](https://github.com/AlexeyGogolev/check-bx-db-value/blob/master/monit_output.png?raw=true)
---
+++ check_bx_db_value-v2.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Site: https://github.com/AlexeyGogolev/check-bx-db-value-v2
+#
+mysql="$(which mysql)" # получение пути к mysql
+php="$(which php)" # получение пути к php
+declare -A CLParams # массив значений параметров КС
+declare -a CLParams_keys # массив ключей для массива параметров КС
+declare -A DBSettings # массив значений переменных конфига
+declare -a DBSettings_keys # массив ключей для массива значений переменных конфига
+DBSettings_keys=(login password database)
+CLParams_keys=(path_to_bxdb_config single_num_value_query max_num_value)
+param_num=0 # счетчик параметров КС
+# получение параметров КС
+for key in "${CLParams_keys[@]}" ; do 
+    ((param_num++))                 
+    CLParams[$key]=${!param_num}    # ${!param_num} здесь генери-т $1 $2... 
+done
+# если нет последнего параметра, показываем справку
+if  [ -z "${CLParams[${CLParams_keys[$param_num-1]}]}" ] ; then
+    printf "Script compares result returned by <${CLParams_keys[1]}> to given <${CLParams_keys[2]}>.\nIf the result more than the given value, then exit with code 1, else exit 0.\n"
+    printf "Usage: \n\t$(basename ${BASH_SOURCE[0]}) " ; for key in "${CLParams_keys[@]}" ; do printf "<$key> "; done ; printf "\n"
+    printf "Example: \n\t$(basename ${BASH_SOURCE[0]}) \"/www/ab.cd/bitrix/.settings.php\" \"select count(id) from b_event where SUCCESS_EXEC<>'Y'\" 5\n" 
+    exit 10
+fi
+# выход если конфиг пустой или его нет 
+
+printf "File ${CLParams[path_to_bxdb_config]} config. \n"
+
+if ! [ -s "${CLParams[path_to_bxdb_config]}" ] ; then 
+    printf "File ${CLParams[path_to_bxdb_config]} doesn't exist or empty.\n"
+    exit 20
+fi
+# выход если параметр запроса к БД содержит команду из "запретного списка"
+echo ${CLParams[single_num_value_query]} | grep -i -q -E 'delete|update|insert|drop' && printf "query \n${CLParams[single_num_value_query]}\nisn't allowed\n" && exit 30
+# получение значений переменных из php-config -n -- без php.ini , -r -- выполнить код без тэгов <?...?>
+for key in "${DBSettings_keys[@]}" ; do 
+  #    DBSettings[$key]="$($php -n -r 'include("'${CLParams[path_to_bxdb_config]}'"); print $'$key';')"
+  DBSettings[$key]="$($php -n -r '$arSet = include("'${CLParams[path_to_bxdb_config]}'"); print_r($arSet["connections"]["value"]["default"]["'${key}'"]);')"
+  printf "\n\t ${DBSettings[$key]} \n"
+done
+
+# экспорт пароля mysql в переменную окружения (по соображениям безопасности)
+export MYSQL_PWD=${DBSettings[password]}
+# получение одиночного значения из БД: -N -- без названий колонок ; -B - (batch) - результаты без "бокса" вокруг значений; -e выполнить запрос
+num_value=`${mysql} -u ${DBSettings[login]} -N -B -e "use ${DBSettings[database]}; ${CLParams[single_num_value_query]}"`
+# отображение значений
+echo "Result of the query (from DB ${DBSettings[database]}): ${num_value}, ${CLParams_keys[2]}: ${CLParams[max_num_value]}"
+# сравнение значений
+
+if [ $num_value -gt ${CLParams[max_num_value]} ]; then
+    exit 1
+fi
